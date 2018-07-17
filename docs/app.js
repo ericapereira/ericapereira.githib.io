eea4b0b3f55b307bd55e680556f0df3017e9ddb8
ericapereira <ericapereira_2020@depauw.edu>
2018-07-16 20:51:17 -0500

    add docs folder to root

---
+++ docs/app.js
@@ -0,0 +1,78 @@
+function goToHome() {
+  document.getElementById('home').style.display = 'block';
+  document.getElementById('VLite').style.display = "none";
+  document.getElementById('Raw').style.display = "none";
+  document.getElementById('Seaborn').style.display = "none";
+  document.getElementById('CivicUS').style.display = "none";
+  document.getElementById('CivicInt').style.display = "none";
+  document.getElementById('Moving').style.display = "none";
+  document.getElementById('FOIA').style.display = "none";
+}
+
+
+function goToVLite() {
+  document.getElementById('home').style.display = "none";
+  document.getElementById('VLite').style.display = 'block';
+  document.getElementById('Raw').style.display = "none";
+  document.getElementById('Seaborn').style.display = "none";
+    document.getElementById('CivicUS').style.display = "none";
+  document.getElementById('CivicInt').style.display = "none";
+  document.getElementById('Moving').style.display = "none";
+  document.getElementById('FOIA').style.display = "none";
+
+}
+
+function goToRaw() {
+  document.getElementById('home').style.display = "none";
+  document.getElementById('CivicUS').style.display = "none";
+  document.getElementById('VLite').style.display = "none";
+  document.getElementById('Raw').style.display = 'block';
+  document.getElementById('Seaborn').style.display = "none";
+  document.getElementById('CivicInt').style.display = "none";
+  document.getElementById('Moving').style.display = "none";
+  document.getElementById('FOIA').style.display = "none";
+}
+function goToCivicUS() {
+  document.getElementById('home').style.display = "none";
+  document.getElementById('VLite').style.display = "none";
+  document.getElementById('Raw').style.display = "none";
+  document.getElementById('Seaborn').style.display = "none";
+  document.getElementById('CivicUS').style.display = 'block';
+  document.getElementById('CivicInt').style.display = "none";
+  document.getElementById('Moving').style.display = "none";
+  document.getElementById('FOIA').style.display = "none";
+}
+
+function goToCivicInt() {
+  document.getElementById('home').style.display = "none";
+  document.getElementById('VLite').style.display = "none";
+  document.getElementById('Raw').style.display = "none";
+  document.getElementById('Seaborn').style.display = "none";
+  document.getElementById('CivicUS').style.display = "none";
+  document.getElementById('CivicInt').style.display = 'block';
+  document.getElementById('Moving').style.display = "none";
+  document.getElementById('FOIA').style.display = "none";
+}
+
+function goToMoving() {
+  document.getElementById('home').style.display = "none";
+  document.getElementById('VLite').style.display = "none";
+  document.getElementById('Raw').style.display = "none";
+  document.getElementById('Seaborn').style.display = "none";
+  document.getElementById('CivicUS').style.display = "none";
+  document.getElementById('CivicInt').style.display = "none";
+  document.getElementById('Moving').style.display = 'block';
+  document.getElementById('FOIA').style.display = "none";
+
+}
+
+function  goToFOIA() {
+  document.getElementById('home').style.display = "none";
+  document.getElementById('VLite').style.display = "none";
+  document.getElementById('Raw').style.display = "none";
+  document.getElementById('Seaborn').style.display = "none";
+  document.getElementById('CivicUS').style.display = "none";
+  document.getElementById('CivicInt').style.display = "none";
+  document.getElementById('Moving').style.display = "none";
+  document.getElementById('FOIA').style.display = 'block';
+}
