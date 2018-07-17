dcb7a15374e74bdec0488091deb935616e24faa3
ericapereira <ericapereira_2020@depauw.edu>
2018-07-16 19:29:16 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+function goToHome() {
+  document.getElementById('feature').style.display = "none";
+  document.getElementById('feature').style.display = "none";
+  document.getElementById('feature').style.display = "none";
+}
+
+function goToFeature() {
+  document.getElementById('feature').style.display = 'block';
+  document.getElementById('home').style.display = "none";
+  document.getElementById('pricing').style.display = "none";
+  document.getElementById('disabled').style.display = "none";
+}
+
+function goToHome() {
+  document.getElementById('feature').style.display = "none";
+  document.getElementById('feature').style.display = "none";
+  document.getElementById('feature').style.display = "none";
+
+}
+
+function goToHome() {
+  document.getElementById('feature').style.display = "none";
+  document.getElementById('feature').style.display = "none";
+  document.getElementById('feature').style.display = "none";
+}
